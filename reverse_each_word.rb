8b8e5bcb28e8086c2a39fe6a41612e884a54e931
Pam Cairo <pcairo_90@outlook.com>
2019-12-21 02:40:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(array)
-new_array = array.split{"")
-reversed_array = new_array.each (|x| x.reverse)
+  new_array = array.split{"")
+  reversed_array = new_array.each (|x| x.reverse)
   return reverse_array.join("")
 end
